69cb79d41f215c2a38cea6a36dd786589f07e8ef
OguzSabitay <oguz.sabitay@supplychainwizard.com>
2021-07-11 16:16:06 +0300

    footer container changed to bigger column and added text-center class

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="site-footer">
     <div class="container">
-        <div class="row footer-row pt-2">
-            <div class="col-sm-6">
+        <div class="row text-center footer-row pt-2">
+            <div class="col-sm-12">
                 <p class="text-justify">
                     Nostalji Plak&trade; is one of the biggest Vinyl Record Player sellers in the world. &copy; 2021-2021
                 </p>
